a121d74a0b25efa078ea86a7b8e3b06f461d6d37
2021sam <7050592@gmail.com>
2023-09-16 02:04:38 -0700

    live detection

--- tradepulse.py
+++ tradepulse.py
@@ -9,50 +9,40 @@ import requests
 import json
 from api_db import DB_Connect
 
-id1 = 1
-id2 = 100
-r = id2 - id1
 stock = 'AAPL'
 
 DBC = DB_Connect()
 
-id = DBC.get_last_id()
-print( {"last_id": id} )
 
-stock = DBC.get_stock(id)
-print( stock )
+class Stats:
+    def __init__(self, symbol):
+        self.mean = 0
+        self.max = 0
+        self.n = 0
+        self.symbol = symbol
+        self.price = []
+        self.changes = []
 
+    def append(self, price):
+        self.price.append(price)
+        self.n = len(self.price)
+        if self.n > 1:
+            change = abs( self.price[-1] - self.price[-2] )
+            self.changes.append(change)
+            self.mean = sum(self.changes) / self.n
 
-# endpoint = f'http://localhost:8000/{stock}/{id1}/{id2}'
-# response_API = requests.get(endpoint)
-# data = response_API.text
-# data = json.loads(data)
-# data = data['stock']
-# # print(data["stock"])
-# # for e in data:
-# #     print(e)
-
-
-# def historic_mean_max(data):
-#     changes = []
-#     max = 0
-#     n = len(data)
-#     # print(f'n = {n}')
-
-#     for i in range(n-1):
-#         # print(f'i = {i}')
-#         # print(data[i])
-#         change = abs( data[i+1][4] - data[i][4] )
-#         changes.append(change)
-
-#         if change > max:
-#             max = change
-
-#     mean = sum(changes) / n
-#     print(f'average = {mean}')
-
-#     return mean, max
+            if change > self.max:
+                self.max = change
 
 
-# max = historic_mean_max(data)
-# print(max)
+stats = Stats('AAPL')
+
+while True:
+    id = DBC.get_last_id()
+    print( {"last_id": id} )
+    stock = DBC.get_stock(id)
+    # print( stock )
+    if stock != stock[2]:
+        continue
+
+    stats.append(stock[5])
