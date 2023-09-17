0755625226a15039b97ee1347332837305a27276
2021sam <7050592@gmail.com>
2023-09-17 16:50:09 -0700

    added max measures

--- tradepulse.py
+++ tradepulse.py
@@ -16,8 +16,16 @@ class Stats:
         self.symbol = symbol
         self.price = []
         self.changes = []
+        self.changes_absolute = []
         self.fatfinger = False
+        self.dead_cat_bounce = False
         self.continuation = False
+
+        self.direction = 0      #   1 -> up, -1 -> down
+        self.continuation_time = 0
+        self.continuation_time_max = 0
+        self.continuation_distance = 0
+        self.continuation_distance_max = 0
         self.long = False
         self.short = False
 
@@ -26,14 +34,33 @@ class Stats:
         self.n = len(self.price)
         if self.n > 1:
             direction = self.price[-1] - self.price[-2]
-            change = abs( self.price[-1] - self.price[-2] )
-            self.changes.append(change)
-            self.mean = sum(self.changes) / (self.n - 1)
+            # change = abs( self.price[-1] - self.price[-2] )
+            change = abs( direction )
+            self.changes.append(direction)
+            self.changes_absolute.append(change)
+            self.mean = sum(self.changes_absolute) / (self.n - 1)
+
+        if self.n > 2:
+            if self.changes[-2] * self.changes[-1] < 0:
+                self.continuation_time = 1
+                self.continuation_distance = self.changes[-1]
+            else:
+                self.continuation_time += 1
+                self.continuation_distance += self.changes[-1]
+
+            if self.continuation_time > self.continuation_time_max:
+                self.continuation_time_max = self.continuation_time
+                print(f'self.time_max: {self.continuation_time_max}')
+
+            if self.continuation_distance > self.continuation_distance_max:
+                self.continuation_distance_max = self.continuation_distance
+                print(f'self.distance_max: {self.continuation_distance_max}')
 
             if change > self.max:
                 self.max = change
                 self.long = direction > 0
                 self.short = direction < 0
+                print(f'self.max = {self.max}')
 
 
 
@@ -42,8 +69,10 @@ live = False
 start_id = 1
 last_live_id = 0
 start_time = time.time()
-lapse_time_seconds = 10
+lapse_time_seconds = 60
 
+
+print('MAX     TIME    DISTANCE')
 
 while start_time + lapse_time_seconds > time.time():
     time.sleep(1)
@@ -68,5 +97,7 @@ while start_time + lapse_time_seconds > time.time():
     stats.append(stock[5])
     # print(stats.price)
     # print(stats.mean, max(stats.changes))
-    if stats.n > 1:
-        print( max(stats.changes) )
+
+    # if stats.n > 1:
+    #     print( max(stats.changes) )
+    #     print(f'max: {stats.max:.2f}, continuation (time, distance): {stats.continuation_time:.2f}, {stats.continuation_distance:.2f}')
